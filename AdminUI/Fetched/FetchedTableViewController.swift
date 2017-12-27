2df947b60cfed1001bffb5352e8ae15ee7346650
Jaanus Siim <jaanus@jaanussiim.com>
2017-12-27 16:37:24 +0200

    Handle core data updates

--- AdminUI/Fetched/FetchedTableViewController.swift
+++ AdminUI/Fetched/FetchedTableViewController.swift
@@ -102,4 +102,40 @@ internal class FetchedTableViewController<Model: NSManagedObject, Cell: UITableV
         try! fetchedController?.performFetch()
         tableView.reloadData()
     }
+    
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch(type) {
+        case NSFetchedResultsChangeType.update:
+            tableView.reloadRows(at: [indexPath!], with: .automatic)
+        case NSFetchedResultsChangeType.insert:
+            tableView.insertRows(at: [newIndexPath!], with: .automatic)
+        case NSFetchedResultsChangeType.delete:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+        case NSFetchedResultsChangeType.move:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+            tableView.insertRows(at: [newIndexPath!], with: .automatic)
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        switch(type) {
+        case .update:
+            tableView.reloadSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .move:
+            fatalError("Wut? \(sectionIndex)")
+        }
+    }
 }
