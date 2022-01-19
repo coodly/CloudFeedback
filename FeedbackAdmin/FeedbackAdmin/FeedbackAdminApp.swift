e993ba46793367fb2e0df9d24717995e2f033b86
Jaanus Siim <jaanus@jaanussiim.com>
2022-01-19 08:00:51 +0200

    Empty admin project

---
+++ FeedbackAdmin/FeedbackAdmin/FeedbackAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackAdminApp.swift
+//  FeedbackAdmin
+//
+//  Created by Jaanus Siim on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
