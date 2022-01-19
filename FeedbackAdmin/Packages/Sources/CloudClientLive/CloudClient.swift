064bf347a5c4810e514c16ba9ead685d1f8af962
Jaanus Siim <jaanus@jaanussiim.com>
2022-01-19 09:50:26 +0200

    Pull messages

--- FeedbackAdmin/Packages/Sources/CloudClientLive/CloudClient.swift
+++ FeedbackAdmin/Packages/Sources/CloudClientLive/CloudClient.swift
@@ -4,9 +4,29 @@ import Logging
 
 extension CloudClient {
     public static func client(with contrainer: CKContainer) -> CloudClient {
+        let database = contrainer.publicCloudDatabase
         func pullMessages(since date: Date) async -> [CKRecord] {
             Log.cloud.debug("Pull messages since: \(date)")
-            return []
+            
+            do {
+                let query = CKQuery(recordType: "Message", predicate: NSPredicate(format: "modificationDate >= %@", date as NSDate))
+                query.sortDescriptors = [NSSortDescriptor(key: "modificationDate", ascending: true)]
+                let (matched, _) = try await database.records(matching: query)
+                var messages = [CKRecord]()
+                for (_, result) in matched {
+                    switch result {
+                    case .success(let record):
+                        messages.append(record)
+                    default:
+                        break
+                    }
+                }
+                Log.cloud.debug("Fetched \(messages.count) messages")
+                return messages
+            } catch {
+                Log.cloud.error(error)
+                fatalError()
+            }
         }
         
         return CloudClient(
